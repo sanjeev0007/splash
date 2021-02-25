8d5b751cbbe740781c417085037abec1cc005871
sanjeev0007 <sonisanjeev650@gmail.com>
2021-02-25 21:46:17 +0530

    Updates

---
+++ precache-manifest.cdb16b53d87fca2a99c2f93ada247222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c85c6e569f6858bb3c66603bbb3841e",
+    "url": "/splash/index.html"
+  },
+  {
+    "revision": "244229f12081bf7b14cd",
+    "url": "/splash/static/css/main.3baabb97.chunk.css"
+  },
+  {
+    "revision": "cb527e25dc5c4974a5f9",
+    "url": "/splash/static/js/2.5484b91a.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/splash/static/js/2.5484b91a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244229f12081bf7b14cd",
+    "url": "/splash/static/js/main.22bcfbb0.chunk.js"
+  },
+  {
+    "revision": "bc0f416a804bf0f7b9ef",
+    "url": "/splash/static/js/runtime-main.26953764.js"
+  }
+]);
